2e0c0c397431b033627cea9142218581574a827e
Summer <coconamy7727@naver.com>
2019-07-26 09:42:46 +0900

    피보나치 시간 줄이기

--- src/study/Fibonacci_1003.java
+++ src/study/Fibonacci_1003.java
@@ -5,25 +5,21 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Fibonacci_1003 {
-	
+
 	static int[][] number; 
-	static boolean[][] numCheck;
 
 	static int getNum(int n,int oz) {
-		if (numCheck[n][oz]) {
+		if (number[n][oz] != 0) {
 			return number[n][oz];
 		}
 		if (n==0) {
 			number[n][0] = 1;
 			number[n][1] = 0;
-			numCheck[n][oz] = true;
 		}else if (n==1) {
 			number[n][0] = 0;
 			number[n][1] = 1;
-			numCheck[n][oz] = true;
 		}else {
 			number[n][oz] = getNum(n-1,oz)+getNum(n-2,oz);
-			numCheck[n][oz] = true;
 		}
 		return number[n][oz];
 	}
@@ -40,12 +36,6 @@ public class Fibonacci_1003 {
 			int N = Integer.parseInt(st.nextToken());
 		
 			number = new int[N+1][2];
-			numCheck = new boolean[N+1][2];
-			
-			for (int i = 0; i<N+1; i++) {
-				numCheck[i][0] = false;
-				numCheck[i][1] = false;
-			}
 			
 			System.out.println(getNum(N,0) +" "+getNum(N,1));
 		}
