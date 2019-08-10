4fb290dfd171bd1347bc758702d29a08292a94b7
Summer <coconamy77@gmail.com>
2019-08-11 01:37:44 +0900

    하노이

---
+++ src/study/Hanoy_11729.java
@@ -0,0 +1,22 @@
+package study;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Hanoy_11729 {
+
+	static int origin = 0;
+	static int cMove(int idx) {
+		
+		return 0;
+	}
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int h = Integer.parseInt(br.readLine());
+		
+		
+		
+	}
+	 
+}
