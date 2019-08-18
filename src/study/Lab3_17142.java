b7e4cbeb493832f6bf2be8380d15f99b106d1600
Summer <coconamy77@gmail.com>
2019-08-19 00:36:08 +0900

    연구소3

--- src/study/Lab3_17142.java
+++ src/study/Lab3_17142.java
@@ -2,54 +2,121 @@ package study;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Lab3_17142 {
 	static int[][] lab;
-	public static int  bfs(int v1, int v2, int v3) {
-		int[] dx = {0,1,0,-1};
-		int[] dy = {1,0,-1,0};
+	static int N;
+	static int[] virus;
+	static int M;
+	
+	public static int bfs(int[] v) {
+		int[] dx = { 0, 1, 0, -1 };
+		int[] dy = { 1, 0, -1, 0 };
+		int[][] tmp = new int[N][N];
+		for (int i = 0; i<N; i++) {
+			for(int j = 0; j<N; j++) {
+				tmp[i][j] = lab[i][j];
+			}
+		}
+		
 		Queue<Integer> q = new LinkedList<Integer>();
-		q.add(v1);
-		q.add(v2);
-		q.add(v3);
+		for(int i = 0; i<M; i++) {
+			q.add(v[i]);
+		}
+
 		
 		while (!q.isEmpty()) {
+			int t = q.poll();
+			//System.out.println(t);
 			
+			for (int d = 0; d<4; d++) {
+				int nx = t/100+dx[d];
+				int ny = t%100+dy[d];
+				
+				if (nx>=0 && ny>=0 && nx<N && ny<N && tmp[nx][ny]==0) {
+					//System.out.println(t);
+					tmp[nx][ny] = tmp[t/100][t%100]+1;
+					
+					if (nx*100+ny>0)
+					q.add(nx*100+ny);
+					
+				}
+			}
 		}
+		System.out.println();
+		int max = 0;
 		
-		return -1;
+		/*for (int i = 0; ;i++) {
+			if (virus[i]==-1) {
+				break;
+			}
+			int p = virus[i];
+			tmp[p/100][p%100] = 1;
+		}*/
+		
+		for (int[] a: tmp) {
+			System.out.println(Arrays.toString(a));
+			for (int b : a) {
+				if (b==0) {
+					return -1;
+				}
+				max = max>b?max:b;
+			}
+		}
+
+		return max;
 	}
-	public static void main(String[] args) throws Exception{
-		
+
+	public static void main(String[] args) throws Exception {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
+
+		N = Integer.parseInt(st.nextToken());
 		lab = new int[N][N];
 		int M = Integer.parseInt(st.nextToken());
-		int[] virus = new int[M];
+		virus = new int[N*N];
 		int idx = 0;
-		for (int i=0; i<N; i++) {
+		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j<N; j++) {
-				lab[i][j] = Integer.parseInt(st.nextToken());
-				if (lab[i][j]==2) {
-					virus[idx++] = i*100+j;
+			for (int j = 0; j < N; j++) {
+				int t = Integer.parseInt(st.nextToken());
+				if (t == 2) {
+					virus[idx++] = i * 100 + j;
+					
+				} else if (t == 1) {
+					lab[i][j] = -1;
 				}
 			}
 		}
-		int max = -1;
-		for (int i=0; i<N-2; i++) {
-			for (int j = i+1; j<N-1; j++) {
-				for (int k = j+1; k<N; k++) {
-					int tmp = bfs(virus[i], virus[j], virus[k]);
-					max = max>tmp?max:tmp;
-				}
-			}
+		
+		virus[idx] = -1;
+		int min = N*N;
+		for (int i = 0; i < idx - (M-1); i++) {
+			int[] v = new int[M];
+			int tmp = dfs(i,v,0);
+			if (tmp!=-1)
+			min = min > tmp ? tmp : min;
 		}
-		System.out.println(max);
+		
+		//int tmp = bfs(virus[0], virus[1], virus[2]);
+		System.out.println(min);
+	}
+	static int dfs(int i,int[] v, int d) {
+		v[d] = virus[i];
+		if (d==M-1) {
+			
+			return bfs(v);
+		}
+			for (int j = i+1; j<N-((M-1)-d);j++) {
+				
+				
+			}
+		
+		
 	}
 }
