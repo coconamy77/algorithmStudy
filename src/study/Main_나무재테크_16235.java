c802b60cc3b9726c6058c9536b77018ce1110b34
student <student@M804>
2019-08-29 17:51:10 +0900

    큐빙

--- src/study/Main_나무재테크_16235.java
+++ src/study/Main_나무재테크_16235.java
@@ -7,36 +7,23 @@ import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
-class Rand {
+class Land {
 	List<Integer> tree = new ArrayList<>();
-	List<Integer> tmptree = new ArrayList<>();
 	int nour;
+	int dead;
+	int lbf;
 
-	public Rand(int nour) {
+	public Land(int nour) {
 		this.nour = nour + 5;
 	}
 
-	public int getNum() {
-
-		// 크기반환 메소드
-		return tree.size();
-	}
-
-	public void sort() {
-		// 정렬메소드
+	public void sort() {// 정렬메소드
 		Collections.sort(tree);
-	}
-
-	public void die(int i) {
-		// i 이상의 나무 삭줴(정렬한 후라 뒤에있는거 걍 삭제하면 됨, 삭제하면서 죽은 나무리스트로 변환)
-		for (int j = i; j < tree.size();) {
-			tmptree.add(tree.remove(j));
-
-		}
 	}
 
 	public void spring() {
 		// 봄메서드
+		dead = tree.size();
 		for (int i = 0; i < tree.size(); i++) {
 			// 정렬 후라 걍 앞에서부터-> 어린순
 			int age = tree.get(i);
@@ -44,57 +31,47 @@ class Rand {
 				nour -= age;// 나이만큼 양분 감소
 				tree.set(i, ++age);// 양분 먹었으면 나이 증가
 			} else {
-				die(i);
+				dead = i;
 				break;
 			}
-
 		}
 	}
 
 	public void summer() {
-		for (int i = 0; i < tmptree.size();) {
-			nour += tmptree.remove(i) / 2;
+		for (int i = dead; i < tree.size();) {
+			nour += tree.remove(i) / 2;
 		}
-	}
-
-	public void fall() {
-		for (int i = 0; i < tmptree.size();) {
-			tree.add(tmptree.remove(i));
-		}
-
-		sort();
+		lbf = tree.size();
 	}
 
 }
 
 public class Main_나무재테크_16235 {
 
-	static Rand[][] map;
+	static Land[][] map;
 	static int[] dx = { 0, 1, 1, 1, 0, -1, -1, -1 };
 	static int[] dy = { 1, 1, 0, -1, -1, -1, 0, 1 };
 	static int N;
 	static int k = 0;
 	static int[][] winter;
-	static List<Integer> rand = new ArrayList<>(N * N);
+	static List<Integer> land = new ArrayList<>(N*N);
 
 	static void fall(int x, int y) {
-
-		for (int t : map[x][y].tree) {
-			if (t % 5 == 0) {
+		for (int t = 0; t < map[x][y].lbf; t++) {
+			if (map[x][y].tree.get(t) % 5 == 0) {
 				for (int d = 0; d < 8; d++) {
 					int nx = x + dx[d];
 					int ny = y + dy[d];
 					if (nx >= 0 && ny >= 0 && nx < N && ny < N) {
 						if (map[nx][ny] == null) {
-							map[nx][ny] = new Rand(k * winter[nx][ny]);
-							rand.add(nx * N + ny);
+							map[nx][ny] = new Land(k * winter[nx][ny]);
+							land.add(nx * N + ny);
 						}
-						map[nx][ny].tmptree.add(1);
+						map[nx][ny].tree.add(1);
 					}
 				}
 			}
 		}
-
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -106,7 +83,7 @@ public class Main_나무재테크_16235 {
 		int K = Integer.parseInt(st.nextToken());
 
 		winter = new int[N][N];// 겨울에 추가되는 양분?
-		map = new Rand[N][N];
+		map = new Land[N][N];
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
@@ -120,57 +97,51 @@ public class Main_나무재테크_16235 {
 			int j = Integer.parseInt(st.nextToken()) - 1;
 			int age = Integer.parseInt(st.nextToken());
 			if (map[i][j] == null) {
-				map[i][j] = new Rand(0);
-				rand.add(i * N + j);
+				map[i][j] = new Land(0);
+				land.add(i * N + j);
 			}
-
 			map[i][j].tree.add(age);
 		}
 
-		for (int i = 0; i < rand.size(); i++) {// 나무가 있는 땅들은 정렬!
-			int x = rand.get(i);
+		for (int i = 0; i < land.size(); i++) {// 나무가 있는 땅들은 정렬!
+			int x = land.get(i);
 			map[x / N][x % N].sort();
-
 		}
 
 		for (; k < K; k++) {
 			// 봄: 나무가 자신의 나이만큼 땅에 있는 양분을 먹고, 나이가 1증가-> 어린나무부터...-> 모자르면 죽음
-			for (int i = 0; i < rand.size(); i++) {
-				int x = rand.get(i);
+			for (int i = 0; i < land.size(); i++) {
+				int x = land.get(i);
 				map[x / N][x % N].spring();
-
 			}
 
 			// 여름 : 죽은 나무가 양분으로 변한다-> 죽은 나무의 나이/2 만큼 추가
-			for (int i = 0; i < rand.size(); i++) {
-				int x = rand.get(i);
+			for (int i = 0; i < land.size(); i++) {
+				int x = land.get(i);
 				map[x / N][x % N].summer();
 			}
 
 			// 가을 : 나무가 번식, 나이가 5의 배수인 나무만 번식하며, 인접한 8개의 칸에 나이가 1인 나무
-			for (int i = 0; i < rand.size(); i++) {
-				int x = rand.get(i);
+			for (int i = 0; i < land.size(); i++) {
+				int x = land.get(i);
 				fall(x / N, x % N);
 
 			}
-
-			// 가을 : 나무가 번식, 나이가 5의 배수인 나무만 번식하며, 인접한 8개의 칸에 나이가 1인 나무
-			for (int i = 0; i < rand.size(); i++) {
-				int x = rand.get(i);
-				map[x / N][x % N].fall();
+			// 추가 후 정렬
+			for (int i = 0; i < land.size(); i++) {
+				int x = land.get(i);
+				map[x / N][x % N].sort();
 			}
 
-			for (int i = 0; i < rand.size(); i++) {
-				int x = rand.get(i);
+			for (int i = 0; i < land.size(); i++) {
+				int x = land.get(i);
 				map[x / N][x % N].nour += winter[x / N][x % N];
-					
 			}
-
 		}
 		int total = 0;
-		for (int i = 0; i < rand.size(); i++) {
-			int x = rand.get(i);
-			total += map[x / N][x % N].getNum();
+		for (int i = 0; i < land.size(); i++) {
+			int x = land.get(i);
+			total += map[x / N][x % N].tree.size();
 		}
 		System.out.println(total);
 
--- src/study/Main_큐빙_5373.java
+++ src/study/Main_큐빙_5373.java
@@ -1,5 +1,106 @@
 package study;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main_큐빙_5373 {
 
+	static int[][][] cube = new int[6][3][3];
+	static int[][] w = { { 4, 3, 5, 2 }, { 0, 5, 1, 4 }, { 0, 2, 1, 3 } };
+
+	// 0:위(흰) 1:아래(노) 2:왼쪽(초) 3:오른쪽(파) 4:앞(빨) 5:뒤(오)
+	static void spin(int i, int d) {
+		// i = 바꾸는 면-> w[i/2], 어느쪽? -> (i%2)*2(0 or 2)
+		// d = 방향? -> -1 or 1
+		int[] tmp = null;
+		switch (i / 2) {
+		case 0:
+			if (d > 0) {
+				int j = (i % 2) * 3;
+				tmp = cube[w[i / 2][j]][(i % 2) * 2];
+				cube[w[i / 2][j]][(i % 2) * 2] = cube[w[i / 2][j + 1]][(i % 2) * 2];
+				j += i*-1;
+				cube[w[i / 2][j]][(i % 2) * 2] = cube[w[i / 2][j + 1]][(i % 2) * 2];
+				j += i*-1;
+				cube[w[i / 2][j]][(i % 2) * 2] = cube[w[i / 2][j + 1]][(i % 2) * 2];
+				j += i*-1;
+				cube[w[i / 2][j]][(i % 2) * 2] = tmp;
+
+			} else {
+				int j = (i % 2-1) * -3;
+				tmp = cube[w[i / 2][j]][(i % 2) * 2];
+				cube[w[i / 2][j]][(i % 2) * 2] = cube[w[i / 2][j + 1]][(i % 2) * 2];
+				j += i*-1;
+				cube[w[i / 2][j]][(i % 2) * 2] = cube[w[i / 2][j + 1]][(i % 2) * 2];
+				j += i*-1;
+				cube[w[i / 2][j]][(i % 2) * 2] = cube[w[i / 2][j + 1]][(i % 2) * 2];
+				j += i*-1;
+				cube[w[i / 2][j]][(i % 2) * 2] = tmp;
+			}
+
+			/*
+			 * for (int j = 0; j<4;j++) { if (j==0) { tmp = cube[w[i/2][j]][0];
+			 * cube[w[i/2][j]][0] = cube[w[i/2][j+1]][0]; }else if (j==3) {
+			 * cube[w[i/2][j]][0] = tmp; }else { cube[w[i/2][j]][0] = cube[w[i/2][j+1]][0];
+			 * } }
+			 */
+			break;
+		case 1:
+			for (int j = 3; j <= 0; j--) {
+				if (j == 3) {
+					tmp = cube[w[i / 2][j]][0];
+					cube[w[i / 2][j]][0] = cube[w[i / 2][j - 1]][0];
+				} else if (j == 0) {
+					cube[w[i / 2][j]][0] = tmp;
+				} else {
+					cube[w[i / 2][j]][0] = cube[w[i / 2][j - 1]][0];
+				}
+			}
+
+		}
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int T = Integer.parseInt(st.nextToken());
+
+		for (int t = 0; t < T; t++) {
+
+			for (int i = 0; i < 6; i++) {
+				for (int j = 0; j < 3; j++) {
+					cube[i][j][0] = i;
+					cube[i][j][1] = i;
+					cube[i][j][2] = i;
+				}
+			}
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+
+			st = new StringTokenizer(br.readLine());
+			for (int n = 0; n < N; n++) {
+				String order = st.nextToken();
+
+				switch (order.charAt(0)) {
+				case 'U':
+					if (order.charAt(1) == '+') {
+
+					}
+
+				case 'D':
+				case 'L':
+				case 'R':
+				case 'F':
+				case 'B':
+
+				}
+
+			}
+
+		}
+
+	}
 }
