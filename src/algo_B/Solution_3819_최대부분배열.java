7272fc84edb0ec665f20d1768a0b0a9646124c06
Summer <coconamy77>
2019-12-09 10:01:44 +0900

    1209

---
+++ src/algo_B/Solution_3819_최대부분배열.java
@@ -0,0 +1,28 @@
+package algo_B;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution_3819_최대부분배열 {
+	
+	
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int t = 1; t<=T; t++) {
+			int N = Integer.parseInt(br.readLine());
+			int[] a = new int[N];
+			
+			for (int n = 0; n<N; n++) {
+				a[n] = Integer.parseInt(br.readLine());
+			}
+			
+			
+			
+			
+		}
+		
+	}
+}
