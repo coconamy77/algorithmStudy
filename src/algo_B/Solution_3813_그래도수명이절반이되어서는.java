85eced308416d3bd93685f57b04a5720dfc75708
Summer <coconamy77>
2019-12-18 12:38:44 +0900

    1218

--- src/algo_B/Solution_3813_그래도수명이절반이되어서는.java
+++ src/algo_B/Solution_3813_그래도수명이절반이되어서는.java
@@ -6,7 +6,6 @@ import java.util.StringTokenizer;
 
 public class Solution_3813_그래도수명이절반이되어서는 {
 	//결정문제!!!!!!!!!
-	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -14,10 +13,66 @@ public class Solution_3813_그래도수명이절반이되어서는 {
 		int T = Integer.parseInt(st.nextToken());
 		for (int t = 1; t<=T; t++) {
 			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int K = Integer.parseInt(st.nextToken());
 			
+			int[] block = new int[N];
+			int[] set  = new int[K];
 			
+			int min = 200008;
+			int max = 0;
 			
+			st = new StringTokenizer(br.readLine());
 			
+			int tmp;
+			
+			for (int n = 0; n<N; n++) {
+				tmp = Integer.parseInt(st.nextToken());
+				
+				min = tmp>min? min:tmp;
+				max = tmp<max? max:tmp;
+				
+				block[n] = tmp;
+			}
+			
+			st = new StringTokenizer(br.readLine());
+			for (int m = 0; m<K; m++) {
+				set[m]	 = Integer.parseInt(st.nextToken());
+			}
+			
+			int idx = 0;
+			int ans = max;
+			int wl = 0,val;
+			while(max!=min) {
+				idx = 0;
+				wl = (max+min)/2;
+				val = 0;
+				for (int n = 0; n<N; n++) {
+					if (block[n]>wl) {
+						val = 0;
+						continue;
+					}else {
+						val++;
+						if(val==set[idx]) {
+							//System.out.println(idx);
+							val = 0;
+							idx++;
+							if (idx==K) {
+								//System.out.println("여기..?"+wl);
+								break;
+							}
+						}
+					}
+				}
+				if (idx==K) {
+					ans = ans<wl? ans: wl;
+					max = wl;
+				}else {
+					//System.out.println("여기는 오는가 여기가 무한?");
+					min = wl+1;
+				}
+			}
+			System.out.println("#"+t+" "+ans);
 			
 		}
 		
--- src/algo_B/Solution_4007_간담회참석.java
+++ src/algo_B/Solution_4007_간담회참석.java
@@ -2,8 +2,8 @@ package algo_B;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution_4007_간담회참석 {
@@ -15,7 +15,7 @@ public class Solution_4007_간담회참석 {
 		int T = Integer.parseInt(st.nextToken());
 		for (int t = 1; t <= T; t++) {
 			st = new StringTokenizer(br.readLine());
-			
+
 			int N = Integer.parseInt(st.nextToken());
 			int M = Integer.parseInt(st.nextToken());
 			int X = Integer.parseInt(st.nextToken()) - 1;
@@ -28,39 +28,50 @@ public class Solution_4007_간담회참석 {
 						.parseInt(st.nextToken());
 
 			}
-fdjdkl;
+
 			int[][] dijk = new int[N][2];
 
 			for (int i = 0; i < N; i++) {
-				if (i==X) continue;
+				if (i == X)
+					continue;
 				dijk[i][0] = Integer.MAX_VALUE;// go
 				dijk[i][1] = Integer.MAX_VALUE;
 
 			}
 
-			Queue<Integer> q = new LinkedList();
+			PriorityQueue<Integer> q = new PriorityQueue<Integer>(new Comparator<Integer>() {
 
-			q.add(X);
+				@Override
+				public int compare(Integer o1, Integer o2) {
+					
+					return Integer.compare(o1%1000, o2%1000);
+				}
+
+				
+			});
+
+			q.add(X*100000);
 
 			while (!q.isEmpty()) {
-				int tmp = q.poll();
-
+				int tmp = q.poll()/100000;
+				
+				
 				for (int i = 0; i < N; i++) {
 					if (map[i][tmp] > 0) {
-						if ( dijk[i][0] > dijk[tmp][0] + map[i][tmp]) {
+						if (dijk[i][0] > dijk[tmp][0] + map[i][tmp]) {
 
 							dijk[i][0] = dijk[tmp][0] + map[i][tmp];
 
-							q.add(i);
+							q.add(i*100000+map[i][tmp]);
 						}
 					}
 				}
 			}
 
-			q.add(X);
+			q.add(X*100000);
 
 			while (!q.isEmpty()) {
-				int tmp = q.poll();
+				int tmp = q.poll()/100000;
 
 				for (int i = 0; i < N; i++) {
 					if (map[tmp][i] > 0) {
@@ -68,12 +79,14 @@ fdjdkl;
 
 							dijk[i][1] = dijk[tmp][1] + map[tmp][i];
 
-							q.add(i);
+							q.add(i*100000+map[tmp][i]);
 						}
 					}
 				}
+				
 			}
 
+			
 			int ans = 0;
 
 			for (int i = 0; i < N; i++) {
