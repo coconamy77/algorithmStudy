93349b1b6ac47278d829bdb2028652e7ff5c2ece
summer <coconamy7727@naver.com>
2020-02-03 18:45:16 +0900

    update

--- src/programmers/Solution_풍선터트리기.java
+++ src/programmers/Solution_풍선터트리기.java
@@ -4,27 +4,21 @@ import java.util.Stack;
 
 public class Solution_풍선터트리기 {
 	public int solution(int[] a) {
-
 		Stack<Integer> stack = new Stack<>();
-
 		int i = 0;
 		stack.add(a[i++]);
 
 		int before = 0, num = 0, next = 0;
-
 		boolean max = false;
 
 		for (; i < a.length - 1; i++) {
 			num = a[i];
 			next = a[i + 1];
 			before = stack.peek();
-//			System.out.println("현재 i: "+i);
-//			System.out.println(before+" "+num+" "+next);
 			if (num > next && num > before) {
 				max = true;
 			}
 			while (max) {
-				//System.out.println(num+"삭제");
 				if (stack.size() == 1) {
 					max = false;
 					num = stack.pop();
@@ -37,17 +31,10 @@ public class Solution_풍선터트리기 {
 				if (num < next || num < before) {
 					max = false;
 				}
-
 			}
 			stack.add(num);
-
 		}
 		stack.add(a[i]);
-//		System.out.println();
-//		for (int n: stack) {
-//			System.out.print(n+" ");
-//		}
-
 		return stack.size();
 	}
 	public static void main(String[] args) {
