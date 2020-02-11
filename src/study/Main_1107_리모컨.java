b614459b18c163943dae67e4399c5e1b8b9e45f2
SummerJung <35861213+coconamy77@users.noreply.github.com>
2020-02-12 08:49:28 +0900

    0212

---
+++ src/study/Main_1107_리모컨.java
@@ -0,0 +1,22 @@
+package study;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1107_리모컨 {
+	static int goal;
+	static boolean[] chk = new boolean[10];
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		goal = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int m = Integer.parseInt(st.nextToken());
+		
+		
+		
+	}
+}
