35e1153d3630a656631211af9dcc0b0b6ea47bf5
Summer <coconamy7727@naver.com>
2020-02-23 12:31:24 +0900

    0223

---
+++ src/study/Main_1081_합.java
@@ -0,0 +1,22 @@
+package study;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1081_합 {
+	static int L,U;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st =new StringTokenizer(br.readLine());
+		
+		L = Integer.parseInt(st.nextToken());
+		U = Integer.parseInt(st.nextToken());
+		
+		for (int i = L; i<=U; i++) {
+			 
+			
+		}
+		
+	}
+}
---
+++ src/study/Main_2309_일곱난쟁이.java
@@ -0,0 +1,53 @@
+package study;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main_2309_일곱난쟁이 {
+	static int[] shorts = new int[9];
+	static boolean fin = false;
+	
+	static void getAns(int dep, int start, int[] ans) {
+		if (fin) return;
+		if (dep==7) {
+			int sum = 0; 
+			for (int i = 0; i<7; i++) {
+				sum += shorts[ans[i]];
+				if (sum >100) return;
+			}
+			if (sum==100) {
+				fin = true;
+			}
+			return;
+		}
+		for (int i = start; i<9; i++) {
+			if (fin) return;
+			ans[dep] = i;
+			getAns(dep+1, start+1,ans);
+			
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		for (int i =0; i<9; i++) {
+			shorts[i] = Integer.parseInt(br.readLine());
+		}
+		
+		int[] ans = new int[7];
+		getAns(0,0,ans);
+		
+		for (int i = 0; i<7; i++) {
+			ans[i] = shorts[ans[i]];
+		}
+		
+		Arrays.sort(ans);
+		
+		for (int i = 0; i<7; i++) {
+			System.out.println(ans[i]);
+		}
+		
+	}
+
+}
