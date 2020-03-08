977a25889bd38645ff7b3e000cc099bf18024de4
summer <coconamy7727@naver.com>
2020-03-08 22:19:33 +0900

    solve 추석트래픽

--- src/programmers/Solution_추석트래픽.java
+++ src/programmers/Solution_추석트래픽.java
@@ -6,7 +6,8 @@ public class Solution_추석트래픽 {
 	public int solution(String[] lines) {
         int answer = 0;
         StringTokenizer st;
-        String time;
+        String[] time;
+        
         for (int i = 0; i<lines.length; i++) {
         	st = new StringTokenizer(lines[i]);
         	
