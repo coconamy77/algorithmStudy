232a64ae35be7677df4c3c54458533b8c5b2b668
summer <coconamy7727@naver.com>
2020-03-08 22:18:10 +0900

    solve 추석트래픽

--- src/programmers/Solution_추석트래픽.java
+++ src/programmers/Solution_추석트래픽.java
@@ -1,8 +1,21 @@
 package programmers;
 
+import java.util.StringTokenizer;
+
 public class Solution_추석트래픽 {
 	public int solution(String[] lines) {
         int answer = 0;
+        StringTokenizer st;
+        String time;
+        for (int i = 0; i<lines.length; i++) {
+        	st = new StringTokenizer(lines[i]);
+        	
+        	st.nextToken();
+        	
+        	time = st.nextToken().split(":");
+        	
+        }
+        
         return answer;
     }
 }
