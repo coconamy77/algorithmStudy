7d3fcf69b7537668da2154287c9e4b8d46a85e5c
Summer <coconamy7727@naver.com>
2020-04-13 00:28:34 +0900

    0413

--- src/study/Main_1057_토너먼트.java
+++ src/study/Main_1057_토너먼트.java
@@ -1,9 +1,29 @@
 package study;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main_1057_토너먼트 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st =new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		int round = 0;
+		
+		while(true) {
+			if (N==Math.pow(2, round)) {
+				break;
+			}
+		}
+		
+		
+		
 		
 	}
-	
-
 }
