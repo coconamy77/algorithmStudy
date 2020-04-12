eb7351df5f3aadddb768398ddc298279dd50d750
Summer <coconamy7727@naver.com>
2020-04-12 19:15:43 +0900

    0412

---
+++ src/test/Solution.java
@@ -0,0 +1,35 @@
+package test;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[][] v) {
+        int[] answer = new int[2];
+        int[] tmp = new int[2];
+        tmp[0] = v[0][0];
+        tmp[1] = v[0][1];
+        for (int i =1; i<3; i++) {
+        	for (int j = 0; j<2; j++) {
+        		if (answer[j]==0) {
+        			answer[j] = v[i][j];
+        		}else {
+        			if (answer[j]==v[i][j]) {
+        				answer[j] = tmp[j];
+        			}else if(tmp[j]!=v[i][j]){
+        				answer[j] = v[i][j];
+        			}
+        		}
+        	}
+        }
+
+        return answer;
+    }
+    
+    public static void main(String[] args) {
+		Solution s= new Solution();
+		System.out.println(Arrays.toString(s.solution(new int[][] {{1,4},{3,4},{3,10}})));
+		System.out.println(Arrays.toString(s.solution(new int[][] {{1,1},{2,2},{1,2}})));
+	}
+	
+	
+}
