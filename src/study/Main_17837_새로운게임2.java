267e2db1c776e4438aa55ae50b4557b829a1038f
coconamy77 <coconamy7727@naver.com>
2020-05-01 19:13:58 +0900

    20200501 solved 새로운게임2

--- src/study/Main_17837_새로운게임2.java
+++ src/study/Main_17837_새로운게임2.java
@@ -38,11 +38,13 @@ class Pawn {
 		this.x = x;
 		this.y = y;
 		if (hasTop()) {
-			top.uad(dep+1,x,y).top = this;
+			(top.uad(dep+1,x,y)).top = this;
+			//System.out.println(top.name+" "+ (top.top).name);
 		}
 		if (dep==0) {
 			this.top = null;
 		}
+		//System.out.println(this.name);
 		return this;
 	}
 
@@ -59,21 +61,23 @@ class Room {
 		this.color = color;
 	}
 
-	public boolean newTop(Pawn top, int newCount) {
-		count += newCount;
-		if (count >= 4) {
+	public boolean newTop(Pawn bottom,Pawn top, int newCount) {
+		if (count+newCount >= 4) {
 			return true;
 		}
-		if (this.count == 0) {
-			this.bottom = top;
+		
+		if (count == 0) {
+			this.bottom = bottom;
 		} else {
-			this.top.top = top;
+			this.top.top = bottom;
 		}
 		this.top = top;
+		count += newCount;
 		return false;
 	}
 
 	public int moved(int name) {
+		//System.out.println("여기 moved: "+name);
 		int cnt = count;
 		if (bottom.name==name) {
 			count = 0;
@@ -94,6 +98,14 @@ class Room {
 		return cnt;
 	}
 	
+	public String toString() {
+		if (bottom==null) {
+			return "[0 0 0 0] ";
+		}else {
+			return "["+bottom.name+" "+bottom.d+" "+top.name+" "+top.d+"] ";
+		}
+				
+	}
 	
 
 }
@@ -104,8 +116,8 @@ public class Main_17837_새로운게임2 {
 	static Pawn[] pawns;
 	static int N, K;
 	
-	static int[] dx = { -1, 0, 1, 0 };
-	static int[] dy = { 0, -1, 0, 1 };
+	static int[] dx = { 0, 0, -1, 1 };
+	static int[] dy = { 1, -1, 0, 0 };
 	
 	
 	static boolean move(Pawn p, boolean isSec) {
@@ -121,20 +133,39 @@ public class Main_17837_새로운게임2 {
 			switch(map[x][y].color) {
 			case 0:
 				p.move(x, y);
-				return map[x][y].newTop(p, map[ox][oy].moved(k));
+				return map[x][y].newTop(p,map[ox][oy].top, map[ox][oy].moved(k));
 			case 1:
 				p.uad(0,x,y);
-				return map[x][y].newTop(p, map[ox][oy].moved(k));
+				return map[x][y].newTop(map[ox][oy].top,p, map[ox][oy].moved(k));
 				
 			default:
 				if (isSec) {
 					return false;
 				}
-				p.d = (p.d+2)%4;
+				if (p.d==0) {
+					p.d = 1;
+				}else if(p.d == 1) {
+					p.d  = 0;
+				}else if (p.d ==2) {
+					p.d = 3;
+				}else {
+					p.d = 2;
+				}
 				return move(p,true);
 			}
 		}else {
-			p.d = (p.d+2)%4;
+			if (isSec) {
+				return false;
+			}
+			if (p.d==0) {
+				p.d = 1;
+			}else if(p.d == 1) {
+				p.d  = 0;
+			}else if (p.d ==2) {
+				p.d = 3;
+			}else {
+				p.d = 2;
+			}
 			return move(p,true);
 		}
 	}
@@ -164,9 +195,10 @@ public class Main_17837_새로운게임2 {
 		int x, y, d;
 		Pawn p;
 		for (int k = 1; k <= K; k++) {
+			
 			st = new StringTokenizer(br.readLine());
-			x = Integer.parseInt(st.nextToken());
-			y = Integer.parseInt(st.nextToken());
+			x = Integer.parseInt(st.nextToken())-1;
+			y = Integer.parseInt(st.nextToken())-1;
 			d = Integer.parseInt(st.nextToken()) - 1;
 
 			p = new Pawn(k, d, x, y);
@@ -176,25 +208,66 @@ public class Main_17837_새로운게임2 {
 			if (map[x][y] == null) {
 				map[x][y] = new Room(0);
 			}
-			map[x][y].newTop(p, 1);
+			map[x][y].newTop(p,p, 1);
 		}
 
 		tmp = 0;
 		while (tmp <= 1000) {
 			tmp++;
+			//System.out.println(tmp+"번째");
+			
+			
 			for (int k = 1; k <= K; k++) {
 				p = pawns[k];
+//				System.out.println(k+"말이 움직");
+//				for (int n = 0; n<N; n++) {
+//					for (int m = 0; m<N; m++) {
+//						if (map[n][m]==null) {
+//							System.out.print("[0 0 0 0] ");
+//						}
+//						else {
+//							System.out.print(map[n][m].toString());
+//						}
+//					}
+//					System.out.println();
+//				}
 				if(move(p,false)) {
-					break;
+					System.out.println(tmp);
+					return;
 				}
 			}
 		}
 
-		if (tmp > 1000) {
 			System.out.println(-1);
-		} else {
-			System.out.println(tmp);
-		}
+		
 	}
 
 }
+
+//
+//0 1 2 0 1 1
+//1 2 0 1 1 0
+//2 1 0 1 1 0
+//1 0 1 1 0 2
+//2 0 1 2 0 1
+//0 2 1 0 2 1
+//
+//→, ←, ↑, ↓
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
