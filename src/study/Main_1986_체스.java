a9cce2e008084a7e363c74f745c9461e1dac5775
Summer <coconamy7727@naver.com>
2020-05-15 18:24:35 +0900

    0515

--- src/study/Main_1986_체스.java
+++ src/study/Main_1986_체스.java
@@ -7,19 +7,17 @@ import java.util.StringTokenizer;
 public class Main_1986_체스 {
 	static int[] dx = { -1, -1, -1, 0, 1, 1, 1, 0 };
 	static int[] dy = { -1, 0, 1, 1, 1, 0, -1, -1 };
-	static int[][] answer, map;
+	static int[][] map;
 	static int N, M;
 
 	static void goQ(int i, int j, int d) {
-		int nx = i + dx[d];
-		int ny = j + dy[d];
-		//System.out.println(nx+" "+ny);
+		i = i + dx[d];
+		j = j + dy[d];
 
-		if (nx > 0 && ny > 0 && nx <= N && ny <= M) {
-			answer[nx][ny] = -1;
-			//System.out.println("들어왔다");
-			if (map[nx][ny] == 0) {
-				goQ(nx, ny, d);
+		if (i > 0 && j > 0 &&i <= N && j <= M) {
+			if (map[i][j] <= 0) {
+				map[i][j] = -1;
+				goQ(i, j, d);
 			}
 		}
 	}
@@ -33,7 +31,6 @@ public class Main_1986_체스 {
 		
 
 		map = new int[N + 1][M + 1];
-		answer = new int[N + 1][M + 1];
 
 		int x, y;
 
@@ -54,7 +51,6 @@ public class Main_1986_체스 {
 		for (int n = 1; n <= N; n++) {
 			for (int m = 1; m <= M; m++) {
 				if (map[n][m] > 0) {
-					answer[n][m] = -1;
 					switch (map[n][m]) {
 					case 1:
 						for (int d = 0; d < 8; d++) {
@@ -65,8 +61,8 @@ public class Main_1986_체스 {
 						for (int d = 0; d < 8; d++) {
 							x = n + kx[d];
 							y = m + ky[d];
-							if (x > 0 && y > 0 && x <= N && y <= M) {
-								answer[x][y] = -1;
+							if (x > 0 && y > 0 && x <= N && y <= M && map[x][y]==0) {
+								map[x][y] = -1;
 							}
 						}
 						break;
@@ -78,8 +74,9 @@ public class Main_1986_체스 {
 		int ans = 0;
 
 		for (int i = 1; i <= N; i++) {
+			
 			for (int j = 1; j <= M; j++) {
-				if (answer[i][j] == 0) {
+				if (map[i][j] == 0) {
 					ans++;
 				}
 			}
