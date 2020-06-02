40a02856f4226c376624f2b00d19b074a9ca46d7
summer <coconamy7727@naver.com>
2020-06-02 20:05:27 +0900

    solve 오픈채팅방

---
+++ src/programmers/Solution_오픈채팅방.java
@@ -0,0 +1,10 @@
+package programmers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution_오픈채팅방 {
+public static void main(String[] args) {
+	Map<String,String> a = new HashMap<>();
+}
+}
