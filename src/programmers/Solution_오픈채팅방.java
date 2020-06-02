eac1ec555c0b86ae1c834c56e240195b916f74db
summer <coconamy7727@naver.com>
2020-06-02 20:08:01 +0900

    update 오픈채팅방

--- src/programmers/Solution_오픈채팅방.java
+++ src/programmers/Solution_오픈채팅방.java
@@ -2,9 +2,45 @@ package programmers;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Solution_오픈채팅방 {
-public static void main(String[] args) {
-	Map<String,String> a = new HashMap<>();
-}
+
+   public String[] solution(String[] record) {
+      String[] answer = {};
+      Map<String, String> map = new HashMap<>();
+      StringTokenizer st;
+      int cnt=0;
+      for (int i = 0; i < record.length; i++) {
+         st = new StringTokenizer(record[i], " ");
+         switch (st.nextToken()) {
+         case "Leave": {
+            cnt++;
+         }
+            break;
+         case "Enter":{
+            cnt++;
+         }
+         default:
+            map.put(st.nextToken(), st.nextToken());
+            break;
+         }
+      }
+      answer = new String[cnt];
+      cnt=0;
+      for(int i=0; i<record.length; i++) {
+         st = new StringTokenizer(record[i], " ");
+         switch (st.nextToken()) {
+         case "Enter": {
+            answer[cnt++]=map.get(st.nextToken())+"님이 들어왔습니다.";
+         }
+            break;
+         case "Leave":
+            answer[cnt++]=map.get(st.nextToken())+"님이 나갔습니다.";
+            break;
+         }
+      }
+      
+      return answer;
+   }
 }
