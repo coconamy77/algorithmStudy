80ee4e40956b35137907d2082ce9efada66bb026
summer <coconamy7727@naver.com>
2020-06-03 20:21:30 +0900

    update reference

--- src/reference/NumberLength.java
+++ src/reference/NumberLength.java
@@ -1,5 +1,20 @@
 package reference;
 
-public class NumberLength {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
+public class NumberLength {
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long num = Long.parseLong(br.readLine());
+		long tmp = num;
+		int loc = 1;
+		while (tmp > 1) {
+			tmp /= 10;
+			loc++;
+		}
+		
+		System.out.println(loc);
+	}
 }
