93a38adaafe25a894d1b4036789faaa150f44ea1
summer <coconamy7727@naver.com>
2020-06-07 21:46:14 +0900

    update 색종이붙이기

--- src/sw/Main_색종이붙이기.java
+++ src/sw/Main_색종이붙이기.java
@@ -9,10 +9,10 @@ public class Main_색종이붙이기 {
 	static int[] cnt = new int[6];
 	static int min = -1;
 
-	public static boolean possible(int x, int y, int v) {
-		for (int i = 0; i < v; i++) {
-			for (int j = 0; j < v; j++) {
-				if (map[x + i][y + j] == 0)
+	public static boolean possible(int x, int y, int size) {
+		for (int i = x; i < x + size; i++) {
+			for (int j = y; j < y + size; j++) {
+				if (map[i][j] == 0)
 					return false;
 			}
 		}
@@ -29,21 +29,25 @@ public class Main_색종이붙이기 {
 
 	public static void findMin(int count) {
 		boolean flag = false;
-		if (min!=-1 && min<count) {
+		if (min!=-1 && min<=count) {
 			return;
 		}
 		for (int i = 0; i < 10; i++) {
 			for (int j = 0; j < 10; j++) {
 				if (map[i][j] == 1) {
 					flag = true;
-					for (int k = 1; k < 6; k++) {
+					for (int k = 5; k > 0; k--) {
 						if (cnt[k] < 5) {
-							if (i + k < 10 && j + k < 10 && possible(i, j, k)) {
+							if (i + k <= 10 && j + k <= 10 && possible(i, j, k)) {
+								//System.out.println(i+", "+j);
 								fill(i, j, k, 0);
 								cnt[k]++;
 								findMin(count + 1);
 								cnt[k]--;
 								fill(i, j, k, 1);
+							}
+							if (count+1==min) {
+								return;
 							}
 						}
 					}
