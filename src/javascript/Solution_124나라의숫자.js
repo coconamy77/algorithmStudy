5cf26dcdeee83e0a8d406e99234900ad9c6548de
summer <coconamy7727@naver.com>
2020-06-20 16:35:02 +0900

    update 124나라의 숫자

--- src/javascript/Solution_124나라의숫자.js
+++ src/javascript/Solution_124나라의숫자.js
@@ -1,16 +1,11 @@
 function solution(n) {
-    let numbers = ['0','1','2','4'];
-    
-    if(n<=3){
+    let numbers = ['1','2','4'];
+    n--;
+    if(n<=2){
         return numbers[n];
     }
     let pre = Math.floor(n/3);
     let now = Math.floor(n%3);
-    
-    if(now===0){
-        now = 3;
-        pre--;
-    }
     
     if(pre<=0){
         return numbers[now];
