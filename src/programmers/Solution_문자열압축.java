6fc274bbf3c5757c9261190aa68ed13da4a7f5ce
summer <coconamy7727@naver.com>
2020-08-24 17:41:06 +0900

    solve 문자열압축

--- src/programmers/Solution_문자열압축.java
+++ src/programmers/Solution_문자열압축.java
@@ -1,18 +1,17 @@
 package programmers;
 
-import java.util.Stack;
-
 class Solution_문자열압축 {
 
 	public int solution(String s) {
-		int answer = 0;
+		int answer = s.length();
 
 		String tmp = "", before = "";
-		int start, length, cnt;
-		boolean first = true;
+		//새로운 문자열, 비교대상 문자열
+		
+		int start, length, cnt; //문자열 시작 위치, 여태까지의 문자열 길이, 해당 문자 반복 횟수
 		for (int i = 1; i < s.length(); i++) {
 			
-			if (answer<i) {
+			if (answer<=i) {
 				break;
 			}
 			
@@ -22,16 +21,18 @@ class Solution_문자열압축 {
 			
 			before = s.substring(start, start + i);
 			start += i;
-			
-			while (start + i < s.length()) {
+			System.out.println(i+"개로 잘랐을 때");
+			while (start + i <= s.length()) {
 				tmp = s.substring(start, start + i);
 				start += i;
 				if (tmp.equals(before)) {
 					cnt++;
 				} else {
 					if (cnt > 1) {
+						System.out.print(cnt);
 						length += 1;
 					}
+					System.out.print(before);
 					length += i;
 					before = tmp;
 					cnt = 1;
@@ -39,17 +40,23 @@ class Solution_문자열압축 {
 			}
 			if (cnt > 1) {
 				length += 1;
+				System.out.print(cnt);
 			}
 			length += i;
-			before = tmp;
-			cnt = 1;
 			
 			length += s.length() - start;
-			if (answer<length) {
+			System.out.println(before +s.substring(start)+"    "+ length);
+			
+			if (answer>length) {
 				answer = length;
 			}
 		}
 
 		return answer;
+	}
+	
+	public static void main(String[] args) {
+		Solution_문자열압축 s = new Solution_문자열압축();
+		System.out.println(s.solution("abcabcabcabcdededededede"));
 	}
 }
