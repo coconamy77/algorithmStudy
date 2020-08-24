f1b8e95b9f747287ebfadd821936748a6d345b2d
summer <coconamy7727@naver.com>
2020-08-24 17:52:19 +0900

    solve 문자열압축

--- src/programmers/Solution_문자열압축.java
+++ src/programmers/Solution_문자열압축.java
@@ -10,8 +10,9 @@ class Solution_문자열압축 {
 		
 		int start, length, cnt; //문자열 시작 위치, 여태까지의 문자열 길이, 해당 문자 반복 횟수
 		for (int i = 1; i < s.length(); i++) {
-			
+			//길이 늘려가면서 짜르기!
 			if (answer<=i) {
+				// 현재 최소값보다 긴 길이는 답이 될 수 없다-> 가지치기
 				break;
 			}
 			
@@ -20,20 +21,30 @@ class Solution_문자열압축 {
 			cnt = 1;
 			
 			before = s.substring(start, start + i);
+			// 제일 처음 문자열
 			start += i;
+			// 다음 문자열 시작 위치 조정 
 			System.out.println(i+"개로 잘랐을 때");
 			while (start + i <= s.length()) {
+				//문자열 조사
+				
 				tmp = s.substring(start, start + i);
+				//자르기!
 				start += i;
 				if (tmp.equals(before)) {
 					cnt++;
+					//전의 문자열과 같으면 갯수 추가
 				} else {
+					// 전의 문자열과 다를때
 					if (cnt > 1) {
+						//만약 1개 이상 있다면 길이 추가 (2a-> 길이: 2, a-> 길이 :1)
 						System.out.print(cnt);
 						length += 1;
 					}
+					//해당 문자열 길이 추가
 					System.out.print(before);
 					length += i;
+					//비교대상 문자열 변환
 					before = tmp;
 					cnt = 1;
 				}
@@ -44,10 +55,13 @@ class Solution_문자열압축 {
 			}
 			length += i;
 			
+			//해당 길이로 짜르고 남은 문자열 체크!
 			length += s.length() - start;
+			//abc-> ab:2 + (3-2)=3
 			System.out.println(before +s.substring(start)+"    "+ length);
 			
 			if (answer>length) {
+				//현재 길이가 답보다 짧을 경우 답 변경
 				answer = length;
 			}
 		}
