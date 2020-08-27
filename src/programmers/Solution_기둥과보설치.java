e002c8a7e48a7754da6a409ca4df5305d5c716bb
summer <coconamy7727@naver.com>
2020-08-27 20:54:52 +0900

    solve 기둥과보설치

--- src/programmers/Solution_기둥과보설치.java
+++ src/programmers/Solution_기둥과보설치.java
@@ -1,5 +1,111 @@
 package programmers;
 
-public class Solution_기둥과보설치 {
+class Solution_기둥과보설치 {
+	static boolean[][][] map;
 
+	public void rm(int x, int y, int w) {
+		map[x][y][w] = false;
+
+		for (int i = 0; i < 4; i++) {
+			if (map[x][y][i]) {
+				return;
+			}
+		}
+		map[x][y][4] = false;
+	}
+
+	public boolean check(int x, int y, int w) {
+		// w==1->보, 0->기둥
+
+		if (w == 0) {
+			if (x != 0 && !map[x][y][4]) {
+				return false;
+			}
+			return true;
+		} else {
+			if (map[x][y][3]) {
+				return true;
+			}
+			if (!map[x][y + 1][4]) {
+				return false;
+			}
+			if (map[x][y + 1][3]) {
+				return true;
+			}
+			if (map[x][y][1] && map[x][y + 1][0]) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public int[][] solution(int n, int[][] build_frame) {
+		int[][] answer = {};
+		map = new boolean[n + 1][n + 1][5];
+		int x, y, a, b;
+		boolean rmb;
+		for (int[] arr : build_frame) {
+			x = arr[1];
+			y = arr[0];
+			a = arr[2];// 0==기둥, 1==보
+			b = arr[3];// 0==삭제, 1==설치
+
+			// 왼보, 오보, 위기, 아기
+			if (b == 1) {
+				if (check(x, y, a)) {
+					map[x][y][4] = true;
+					if (a == 0) {
+						map[x][y][2] = true;
+						map[x + 1][y][4] = true;
+						map[x + 1][y][3] = true;
+					} else {
+						map[x][y][0] = true;
+						map[x][y + 1][4] = true;
+						map[x][y + 1][1] = true;
+					}
+				}
+			} else {
+				if (a == 0) {
+					rm(x, y, 2);
+					rm(x + 1, y, 3);
+					rmb = true;
+					for (int i = 0; i < 3; i++) {
+						if (map[x+1][y][i]) {
+							if (i == 0) {
+								if (!check(x+1, y, i)) {
+									rmb = false;
+									break;
+								}
+							}else if (i==1) {
+								if (!check(x+1, y-1, i)) {
+									rmb = false;
+									break;
+								}
+							}else if(i==2) {
+								if (!check(x+1,y,i)) {
+									rmb = false;
+									break;
+								}
+							}
+						}
+					}
+					if (!rmb) {
+						map[x][y][4] = true;
+						map[x][y][2] = true;
+						map[x + 1][y][4] = true;
+						map[x + 1][y][3] = true;
+					}
+				}
+			}
+
+		}
+		
+		for (int i = 0; i<n; i++) {
+			for (int j = 0; j<n; j++) {
+				
+			}
+		}
+
+		return answer;
+	}
 }
