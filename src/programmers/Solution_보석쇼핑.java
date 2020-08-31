06325702f643f1680f48de58a4f4f4d74221b72d
summer <coconamy7727@naver.com>
2020-08-31 20:48:31 +0900

    solve 보석쇼핑

--- src/programmers/Solution_보석쇼핑.java
+++ src/programmers/Solution_보석쇼핑.java
@@ -1,8 +1,31 @@
 package programmers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution_보석쇼핑 {
 	public int[] solution(String[] gems) {
-        int[] answer = {};
-        return answer;
+       // int[] answer = new int[2];
+        
+        List<String> list = new ArrayList<>();
+        list.add(gems[0]);
+        int last = 0,first = 0;
+        for (int i = 1; i<gems.length; i++) {
+        	if(!list.contains(gems[i])) {
+        		while(last<i) {
+        			list.add(gems[++last]);
+        		}
+        	}
+        }
+        
+        //String tmp = list.remove(0);
+        while(list.contains(list.remove(0))) {
+        	first++;
+        	
+        	//tmp = list.remove(0);
+        }
+        
+        
+        return new int[]{first+1,last+1};
     }
 }
