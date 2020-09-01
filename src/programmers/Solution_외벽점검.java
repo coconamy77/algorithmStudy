d414f86a2bc4496a0a9951d4ad3e6733a03867c6
SummerJung <35861213+coconamy77@users.noreply.github.com>
2020-09-01 20:30:37 +0900

    Create Solution_외벽점검.java

---
+++ src/programmers/Solution_외벽점검.java
@@ -0,0 +1,20 @@
+class Solution_외벽점검{
+	
+	public int solution(int n, int[] weak, int[] dist) {
+        int answer = -1;
+        int x = dist[dist.length-1];
+        for (int i = 0; i<)
+        
+        
+        return answer;
+    }
+	
+	
+    public static void main(String args[]){
+
+        
+    }
+}
+
+
+
