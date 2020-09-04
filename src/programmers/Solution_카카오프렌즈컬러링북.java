e27a7222884954e87974a38ed50db7404ad80045
SummerJung <35861213+coconamy77@users.noreply.github.com>
2020-09-04 19:21:09 +0900

    Update Solution_카카오프렌즈컬러링북.java

--- src/programmers/Solution_카카오프렌즈컬러링북.java
+++ src/programmers/Solution_카카오프렌즈컬러링북.java
@@ -1,32 +1,47 @@
 import java.util.*;
 
-public class Solution_카카오프렌즈컬러링북{
-   public int[] solution(int m, int n, int[][] picture) {
+class Solution {
+       public int[] solution(int m, int n, int[][] picture) {
         int numberOfArea = 0;
         int maxSizeOfOneArea = 0;
 		
-		int dx = {0,0,1,-1};
-		int dy = {1,-1,0,0};
+		int[] dx = {0,0,1,-1};
+		int[] dy = {1,-1,0,0};
 		
 		Queue<Integer> q = new LinkedList<>();
-		int tmp = 0, tmpArea = 0, x,y;
+		int tmp = 0, tmpArea = 0, x,y,nx,ny;
 		for (int i = 0; i<m; i++){
 			for (int j = 0; j<n; j++){
-				if (picture[i][j]	!= 0){
+				if (picture[i][j]!= 0){
 					tmp = picture[i][j];
 					
 					numberOfArea++;
-					q.add(i*1000+j);
+					q.add(i*10000+j);
 					picture[i][j] = 0;
+					tmpArea++;
 					
 					while(!q.isEmpty()){
 						x = q.poll();
+						y = x%10000;
+						x = x/10000;
 						
+						for (int d = 0; d<4; d++){
+							nx = x+dx[d];
+							ny = y+dy[d];
+							
+							if (nx>=0 && ny>=0 && nx<m && ny<n && picture[nx][ny]==tmp){
+								tmpArea++;
+								picture[nx][ny] = 0;
+								q.add(nx*10000+ny);
+							}
+						}
 						
 						
 					}
 					
+					maxSizeOfOneArea = maxSizeOfOneArea>tmpArea?maxSizeOfOneArea:tmpArea;
 					
+					tmpArea = 0;
 					
 					
 					
@@ -42,12 +57,4 @@ public class Solution_카카오프렌즈컬러링북{
         return answer;
     }
 
-
-    public static void main(String args[]){
-
-        
-    }
 }
-
-
-
