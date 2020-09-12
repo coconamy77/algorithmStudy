99f4e746a8206f4351eb10104734b13b1416af30
summer <coconamy7727@naver.com>
2020-09-12 19:21:25 +0900

    solve 삼각달팽이

--- src/programmers/Solution_경주로건설.java
+++ src/programmers/Solution_경주로건설.java
@@ -2,36 +2,37 @@ package programmers;
 
 public class Solution_경주로건설 {
 
-   static int board2[][];
-   static int dx[] = {0, 0, 1, -1};
-   static int dy[] = {1, -1, 0, 0};
-   static int nx, ny;
-   static int plusScore;
-   static int length;
-   public static void DFS(int x, int y, int direction) {
-   
-      for(int i=0; i<4; i++) {
-         nx = dx[i]+x;
-         ny = dy[i]+y;
-         if(direction==i) {
-            plusScore=100;
-         }else {
-            plusScore=600;
-         }
-         if(nx>=0 && ny>=0 && nx<board2.length && ny<board2.length && board2[nx][ny]!=1 
-               && (board2[nx][ny]>=board2[x][y]+plusScore || board2[nx][ny]==0) && (nx!=0 || ny!=0)) {
-            board2[nx][ny]=board2[x][y]+plusScore;
-            DFS(nx, ny, i);
-         }
-      }
-   }
-   
-      public int solution(int[][] board) {
-         board2= board;
-         length=board2.length;
-            for(int i=0; i<4; i++){
-             DFS(0, 0, i);
-            }
-         return board2[length-1][length-1];
-      }
-   }
+	static int board2[][];
+	static int dx[] = { 0, 0, 1, -1 };
+	static int dy[] = { 1, -1, 0, 0 };
+	static int nx, ny;
+	static int plusScore;
+	static int length;
+
+	public static void DFS(int x, int y, int direction) {
+
+		for (int i = 0; i < 4; i++) {
+			nx = dx[i] + x;
+			ny = dy[i] + y;
+			if (direction == i) {
+				plusScore = 100;
+			} else {
+				plusScore = 600;
+			}
+			if (nx >= 0 && ny >= 0 && nx < board2.length && ny < board2.length && board2[nx][ny] != 1
+					&& (board2[nx][ny] >= board2[x][y] + plusScore || board2[nx][ny] == 0) && (nx != 0 || ny != 0)) {
+				board2[nx][ny] = board2[x][y] + plusScore;
+				DFS(nx, ny, i);
+			}
+		}
+	}
+
+	public int solution(int[][] board) {
+		board2 = board;
+		length = board2.length;
+		for (int i = 0; i < 4; i++) {
+			DFS(0, 0, i);
+		}
+		return board2[length - 1][length - 1];
+	}
+}
---
+++ src/programmers/Solution_삼각달팽이.java
@@ -0,0 +1,8 @@
+package programmers;
+
+public class Solution_삼각달팽이 {
+    public int[] solution(int n) {
+        int[] answer = {};
+        return answer;
+    }
+}
