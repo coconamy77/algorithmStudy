661def48723aa7f1fce892565f6f857cc11b54e9
summer <coconamy7727@naver.com>
2020-09-17 23:46:26 +0900

    create 청소년상어.java

--- src/sw/Main_19236_청소년상어.java
+++ src/sw/Main_19236_청소년상어.java
@@ -16,15 +16,15 @@ class Fish {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public void move(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public void turn() {
 		dir++;
-		if (dir==8) {
+		if (dir == 8) {
 			dir = 0;
 		}
 	}
@@ -40,26 +40,27 @@ public class Main_19236_청소년상어 {
 	static int answer = 0;
 
 	static boolean range(int x, int y) {
-		if (x>=0 && y>=0 && x<4 && y<4) {
+		if (x >= 0 && y >= 0 && x < 4 && y < 4) {
 			return true;
 		}
 		return false;
 	}
-	
+
 	static void getNew(Fish f) {
 		int nx, ny;
-		nx = f.x+dx[f.dir];
-		ny = f.y+dy[f.dir];
-		
-		while(!range(nx,ny) || map[nx][ny]==fish[0] ) {
+		nx = f.x + dx[f.dir];
+		ny = f.y + dy[f.dir];
+
+		while (!range(nx, ny) || map[nx][ny] == fish[0]) {
 			f.turn();
-			nx = f.x+dx[f.dir];
-			ny = f.y+dy[f.dir];
+			nx = f.x + dx[f.dir];
+			ny = f.y + dy[f.dir];
 		}
 		f.move(nx, ny);
 	}
-	
+
 	static void moveFish() {
+		System.out.println("물고기 움직여");
 		Fish f;
 		int ox, oy, nx, ny;
 		for (int i = 1; i < 17; i++) {
@@ -72,20 +73,20 @@ public class Main_19236_청소년상어 {
 			getNew(f);
 			nx = f.x;
 			ny = f.y;
-			if (map[nx][ny]!=null) {
+			if (map[nx][ny] != null) {
 				map[ox][oy] = map[nx][ny];
-				map[ox][oy].move(ox,oy);
+				map[ox][oy].move(ox, oy);
 			}
 			map[nx][ny] = f;
 		}
 	}
-	
+
 	public static int movable() {
 		int m = 0;
 		int nx, ny;
-		for (int i = 1; i<4;i++ ) {
-			nx = sx+dx[sd]*i;
-			ny = sy+dy[sd]*i;
+		for (int i = 1; i < 4; i++) {
+			nx = sx + dx[sd] * i;
+			ny = sy + dy[sd] * i;
 			if (!range(nx, ny)) {
 				return m;
 			}
@@ -93,35 +94,76 @@ public class Main_19236_청소년상어 {
 		}
 		return m;
 	}
-	
-	public static int getAnswer() {
-		int answer = 0;
-		int movable;
-		while((movable= movable())>0) {
-			for (int m = 1; m<=movable; m++) {
-				moveShark(m);
+
+	public static void copymap(Fish[][] ori, Fish[][] tmp) {
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				tmp[i][j] = ori[i][j];
 			}
 		}
-		
-		return answer;
 	}
-	
-	public static Fish moveShark(int m) {
-		Fish f = new Fish(22, 2, 2, 2);
+
+	public static void copyFish(Fish[] ori, Fish[] tmp) {
+		for (int i = 0; i < 17; i++) {
+			tmp[i] = ori[i];
+		}
+	}
+
+	public static void getAnswer(int sum) {
+		int movable = movable();
+		if (movable == 0) {
+			if (sum > answer)
+				answer = sum;
+			return;
+		}
+		Fish[][] tmp = new Fish[4][4];
+		copymap(map, tmp);
+		Fish[] tmpfish = new Fish[17];
+		copyFish(fish, tmpfish);
+		int ox = sx, oy = sy, od = sd;
+		int tmpval = 0;
+		for (int m = 1; m <= 3; m++) {
+			if (map[sx+dx[sd]*m][sy+dy[sd]*m]==null) {
+				continue;
+			}
+			tmpval = moveShark(sx+dx[sd]*m,sy+dy[sd]*m);
+			moveFish();
+			getAnswer(sum + tmpval);
+			sx = ox;
+			sy = oy;
+			sd = od;
+			copymap(tmp, map);
+			copyFish(tmpfish, fish);
+		}
+	}
+
+	public static int moveShark(int nx, int ny) {
+		int val = 0;
+
+		map[sx][sy] = null;
 		
-		
-		
-		return f;
+		sx = nx; 
+		sy = ny;
+
+		val = map[sx][sy].name;
+		map[sx][sy] = fish[0];
+		sd = fish[val].dir;
+		fish[0].dir = sd;
+		fish[val] = null;
+		fish[0].move(sx, sy);
+
+		return val;
 	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
 		int n, d;
 		for (int i = 0; i < 4; i++) {
+			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < 4; j++) {
 				n = Integer.parseInt(st.nextToken());
-				d = Integer.parseInt(st.nextToken());
+				d = Integer.parseInt(st.nextToken())-1;
 				fish[n] = new Fish(n, d, i, j);
 				map[i][j] = fish[n];
 			}
@@ -130,11 +172,11 @@ public class Main_19236_청소년상어 {
 		answer = map[0][0].name;
 		fish[0] = new Fish(0, sd, 0, 0);// 상어는 0번!!
 		map[0][0] = fish[0];
+		fish[answer] = null;
 
 		moveFish();
-		
-		answer += getAnswer();
-		
+
+		getAnswer(answer);
 
 	}
 }
