4c8755d4df4047b8c58b08faf811f6c97ab4a18e
summer <coconamy7727@naver.com>
2020-10-02 19:34:26 +0900

    update numtokor

--- src/practice/NumToKor.java
+++ src/practice/NumToKor.java
@@ -2,40 +2,56 @@ package practice;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class NumToKor {
 
 	public static void main(String[] args) throws Exception{
-		String[][] kor = {{"","일","이","삼","사","오","육","칠","팔","구"},{"","십","백","천","만"}};//0,1,2,3,4
+		String[][] kor = {{"","일","이","삼","사","오","육","칠","팔","구"},{"","십","백","천"},{"","만","억","조"}};//0,1,2,3,4
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int num = Integer.parseInt(br.readLine());
+		long num = Long.parseLong(br.readLine());
 		String answer = "";
-		int div = 4;
+		int[] div = {0,0};//십백천, 만억조
 		int now = 0;
-		int tmp;
+		long tmp = num;
+		int loc = 1;
 		
-		while(div>=0) {
-			tmp=(int) Math.pow(10, div);
-			
-			now = num/tmp;
+		while(tmp>1) {
+			tmp/=10;
+			div[0]++;
+			loc++;
+			if (div[0]==4) {
+				div[1]++;
+				div[0]=0;
+			}
+		}
+		
+		while(loc>0) {
+			tmp= (long) Math.pow(10, --loc);
+			now = (int) (num/tmp);
 			num %=tmp;
 			
 			if (now==0) {
-				div--;
+				div[0]--;
+				if (div[1]!=0 && div[0]==-1) {
+					answer+=kor[2][div[1]];
+					div[1]--;
+					div[0]=3;
+				}
 				continue;
 			}
-			if (now==1) {
-				if (div==0) {
-					answer += kor[0][1];
-				}
-			}else {
-				answer +=kor[0][now];
+			answer +=kor[0][now];
+			
+			answer += kor[1][div[0]];
+			
+			div[0]--;
+			if (div[1]!=0 && div[0]==-1) {
+				answer+=kor[2][div[1]];
+				div[1]--;
+				div[0]=3;
 			}
-			
-			answer += kor[1][div--];
 			System.out.println(answer);
-			
 			
 		}
 		System.out.println(answer);
