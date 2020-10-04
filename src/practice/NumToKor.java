a5fd64bb666b1f303b935255d8b8f1e7541b999e
summer <coconamy7727@naver.com>
2020-10-04 20:19:12 +0900

    update numtokor

--- src/practice/NumToKor.java
+++ src/practice/NumToKor.java
@@ -6,55 +6,61 @@ import java.util.Arrays;
 
 public class NumToKor {
 
-	public static void main(String[] args) throws Exception{
-		String[][] kor = {{"","일","이","삼","사","오","육","칠","팔","구"},{"","십","백","천"},{"","만","억","조"}};//0,1,2,3,4
-		
+	public static void main(String[] args) throws Exception {
+		String[][] kor = { { "", "일", "이", "삼", "사", "오", "육", "칠", "팔", "구" }, { "", "십", "백", "천" },
+				{ "", "만", "억", "조" } };// 0,1,2,3,4
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		long num = Long.parseLong(br.readLine());
 		String answer = "";
-		int[] div = {0,0};//십백천, 만억조
+		int[] div = { 0, 0 };// 십백천, 만억조
 		int now = 0;
 		long tmp = num;
 		int loc = 1;
-		
-		while(tmp>1) {
-			tmp/=10;
+
+		while (tmp > 1) {
+			tmp /= 10;
 			div[0]++;
 			loc++;
-			if (div[0]==4) {
+			if (div[0] == 4) {
 				div[1]++;
-				div[0]=0;
+				div[0] = 0;
 			}
 		}
-		
-		while(loc>0) {
-			tmp= (long) Math.pow(10, --loc);
-			now = (int) (num/tmp);
-			num %=tmp;
-			
-			if (now==0) {
+
+		while (loc > 0) {
+			tmp = (long) Math.pow(10, --loc);
+			now = (int) (num / tmp);
+			num %= tmp;
+
+			if (now == 0) {
 				div[0]--;
-				if (div[1]!=0 && div[0]==-1) {
-					answer+=kor[2][div[1]];
+				if (div[1] != 0 && div[0] == -1) {
+					answer += kor[2][div[1]];
 					div[1]--;
-					div[0]=3;
+					div[0] = 3;
 				}
 				continue;
 			}
-			answer +=kor[0][now];
-			
+			if (now == 1) {
+				if (div[0] == 0 && div[1] == 0) {
+					answer += kor[0][now];
+				}
+			} else {
+				answer += kor[0][now];
+			}
 			answer += kor[1][div[0]];
-			
+
 			div[0]--;
-			if (div[1]!=0 && div[0]==-1) {
-				answer+=kor[2][div[1]];
+			if (div[1] != 0 && div[0] == -1) {
+				answer += kor[2][div[1]];
 				div[1]--;
-				div[0]=3;
+				div[0] = 3;
 			}
 			System.out.println(answer);
-			
+
 		}
 		System.out.println(answer);
-		
+
 	}
 }
