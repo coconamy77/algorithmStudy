c18f07ecbae5b3125af82dba27f4c2ee15ff33e1
summer <coconamy7727@naver.com>
2020-12-31 21:12:41 +0900

    solved 비밀지도

---
+++ src/programmers/Solution_비밀지도.java
@@ -0,0 +1,28 @@
+package programmers;
+
+public class Solution_비밀지도 {
+	public String getAnswer(int number,int n){
+        StringBuilder sb = new StringBuilder();
+        while(n-->0){
+            if(number%2==0){
+                sb.insert(0," ");
+            }else{
+                sb.insert(0,"#");
+            }
+            number/=2;
+        }
+        return sb.toString();
+    }
+    
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        
+        for(int i = 0; i<n; i++){
+            int number = arr1[i] | arr2[i];
+            System.out.println(number);
+            answer[i] = getAnswer(number,n);
+        }
+        
+        return answer;
+    }
+}
