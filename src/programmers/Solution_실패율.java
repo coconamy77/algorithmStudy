8f864cfc617cbca8a39df4b115584471c29afc6d
coconamy77 <coconamy7727@naver.com>
2021-01-04 14:20:22 +0900

    create 실패율

---
+++ src/programmers/Solution_실패율.java
@@ -0,0 +1,15 @@
+package programmers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution_실패율 {
+	class Solution {
+	    public int[] solution(int N, int[] stages) {
+	        int[] answer = {};
+	        
+	        List<Integer> list = new ArrayList<>();
+	        return answer;
+	    }
+	}
+}
