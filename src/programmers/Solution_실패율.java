fd66397a9002e9740891d95abb8e5e11b7f59d78
coconamy77 <coconamy7727@naver.com>
2021-04-17 18:50:46 +0900

    실패율

--- src/programmers/Solution_실패율.java
+++ src/programmers/Solution_실패율.java
@@ -1,14 +1,31 @@
 package programmers;
 
-import java.util.ArrayList;
-import java.util.List;
+class Stage implements Comparable<Stage>{
+	int stage;
+    int players;
+    int fail;
+    
+    double fRate;
 
+	@Override
+	public int compareTo(Stage o) {
+		if(this.fRate>o.fRate) {
+			return -1;
+		}
+		if(o.fRate>this.fRate) {
+			return 1;
+		}
+		if(this.stage<o.stage) {
+			return -1;
+		}
+		return 1;
+	}
+}
 public class Solution_실패율 {
 	class Solution {
 	    public int[] solution(int N, int[] stages) {
 	        int[] answer = {};
 	        
-	        List<Integer> list = new ArrayList<>();
 	        return answer;
 	    }
 	}
