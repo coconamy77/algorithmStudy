914a272460a6950c5cd77514bc322e8bb3a95477
coconamy77 <coconamy7727@naver.com>
2021-04-23 20:45:39 +0900

    solved완주하지 못한선수

--- src/programmers/Solution_완주하지못한선수.java
+++ src/programmers/Solution_완주하지못한선수.java
@@ -1,21 +1,31 @@
 package programmers;
 
 import java.util.HashMap;
+import java.util.Map;
 
 class Solution_완주하지못한선수 {
    public String solution(String[] participant, String[] completion) {
-        String answer = "";
-		HashMap<String, Integer> hm = new HashMap<>();
-		for (String player : participant)
-			hm.put(player, hm.getOrDefault(player, 0) + 1);
-		for (String player : completion)
-			hm.put(player, hm.get(player) - 1);
-
-		for (String key : hm.keySet()) {
-			if (hm.get(key) != 0) {
-				answer = key;
-			}
-		}
-		return answer;
+	   String answer = "";
+       Map<String, Integer> map = new HashMap<>();
+       for(String name: completion){
+           if(map.containsKey(name)){
+               map.replace(name,map.get(name)+1);
+           }else{
+               map.put(name, 1);
+           }
+       }
+       
+       for(String name: participant){
+           if(!map.containsKey(name)){
+               return name;
+           }
+           int c = map.get(name);
+           if(c==1){
+               map.remove(name);
+           }else{
+                map.replace(name,c-1);
+           }
+       }
+       return answer;
    }
 }
