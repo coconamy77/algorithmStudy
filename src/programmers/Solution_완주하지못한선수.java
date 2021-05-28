58299a6f8ed1178cdc4d497ddf1f122590a5ee03
coconamy77 <coconamy7727@naver.com>
2021-05-29 00:05:16 +0900

    solved 완주하지 못한 선수

--- src/programmers/Solution_완주하지못한선수.java
+++ src/programmers/Solution_완주하지못한선수.java
@@ -28,4 +28,28 @@ class Solution_완주하지못한선수 {
        }
        return answer;
    }
+   
+   public String solution2(String[] participant, String[] completion) {
+       String answer = "";
+       Map<String, Integer> map = new HashMap<>();
+       
+       for(String p: participant){
+           map.put(p, map.getOrDefault(p, 0)+1);
+       }
+       
+       for(String c: completion){
+           int tmp = map.get(c);
+           
+           if(tmp==1){
+               map.remove(c);
+           }else{
+               map.replace(c, tmp-1);
+           }
+       }
+       
+       answer = (String)map.keySet().toArray()[0];
+       
+       return answer;
+   }
+   
 }
